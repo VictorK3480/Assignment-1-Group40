110ad29e30f68dc1d2d976405b207389cd3442e5
Hannah <s204737@dtu.dk>
2025-09-29 21:10:29 +0200

    added stuff

--- src/data_ops/__init__.py
+++ src/data_ops/__init__.py
@@ -1,3 +1,3 @@
-from .data_loader import DataLoader
-from .data_processor import DataProcessor
-from .data_visualizer import DataVisualizer
+from .data_loader import DataLoader1a, DataLoader1b
+#from .data_processor import DataProcessor
+#from .data_visualizer import DataVisualizer

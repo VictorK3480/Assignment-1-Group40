4257c703762f23fe3fd8f78ad5c2fde437d3a00b
Hannah <s204737@dtu.dk>
2025-10-02 15:20:45 +0200

    added b and c

--- src/runner/__init__.py
+++ src/runner/__init__.py
@@ -1,1 +1,1 @@
-from .runner import run_optimization_1a, run_optimization_1b, run_export_tariff_sweep, run_buying_price_sweep
+from .runner import run_optimization_1a, run_optimization_1b, run_export_tariff_sweep, run_buying_price_sweep, run_optimization_1c
